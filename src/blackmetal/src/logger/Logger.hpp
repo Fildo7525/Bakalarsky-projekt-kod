8e32b6f45943236f548a9ce67be926a37c0f6105
Filip Lobpreis <f.lobpreis@gmail.com>
2022-10-29 22:55:40 +0200

    log: Adding comments
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/blackmetal/src/logger/Logger.hpp
+++ src/blackmetal/src/logger/Logger.hpp
@@ -20,9 +20,15 @@ enum class dbg_level {
  * @class Logger
  * @brief Class handelling all the debugging from the macros in @file log.hpp
  *
+ * The ros2 logger does not support the functionality that is in this file.
+ * The buildint logger would have to defile the modules on every write and will dump
+ * all the logs to the same file. The structure is different, too. Therefor I have created 
+ * a Logger class that suits my needs.
+ *
  * TODO: The logging level could be changed during the runtime.
  * There could be a file containing names of all the modules with a default logging level.
  * The logging level could be taken from the file on every print or just on initialization.
+ *
  * WARN: This will slower the program significantly.
  * This would be better with QT5 and FileWatcher.
  */
@@ -32,14 +38,21 @@ public:
 	/// Constructor
 	explicit Logger(const char *module, dbg_level lvl = dbg_level::INFO);
 
-	const char* dbgLevelToString(const dbg_level level);
+	/**
+	 * @brief Converts the log level to a specified string.
+	 *
+	 * @param level Log level to be converted.
+	 * @return log level name in string type.
+	 */
 	static const char* dbgLevelToString(const dbg_level level);
 
 	/**
 	 * @brief Log message at a specific codePath with a colour to the screen.
 	 *
 	 * The same log is printed to the file named according to the module name. In the file
-	 * is a timestamp at the beginnging of the logging message.
+	 * is a timestamp at the beginnging of the logging message. This method is counting with
+	 * multiple threads writing to the standard output and to the same file. Thus the 
+	 * printing is guarded with scoped mutex.
 	 *
 	 * @param codePath Function name where the logging message is supported.
 	 * @param pid Process printing the message.
@@ -55,6 +68,9 @@ public:
 	 */
 	dbg_level level();
 
+	/**
+	 * @brief Returns the name of the module.
+	 */
 	std::string moduleName();
 
 	/**
