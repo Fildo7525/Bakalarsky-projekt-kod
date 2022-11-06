a4b16c3f094333953795b606f058677cddfe1f1a
Filip Lobpreis <f.lobpreis@gmail.com>
2022-11-06 22:18:40 +0100

    stopwatch: measuring time only in debug mode
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

---
+++ src/blackmetal/src/stopwatch/Stopwatch.cpp
@@ -0,0 +1,48 @@
+#include "Stopwatch.hpp"
+#include <chrono>
+#include <mutex>
+
+static std::vector<double> stoppedTimes = std::vector<double>();
+
+Stopwatch::Stopwatch()
+	: m_start(std::chrono::high_resolution_clock::now())
+{
+}
+
+Stopwatch::~Stopwatch()
+{
+	auto diff = std::chrono::high_resolution_clock::now() - m_start;
+	double diffMiliseconds = std::chrono::duration_cast<std::chrono::microseconds>(diff).count();
+	std::mutex mut;
+	{
+		std::lock_guard<std::mutex> lock(mut);
+		stoppedTimes.push_back(diffMiliseconds);
+	}
+}
+
+const double &Stopwatch::lastStoppedTime()
+{
+	std::mutex mut;
+	{
+		std::lock_guard<std::mutex>lock(mut);
+		return stoppedTimes.back();
+	}
+}
+
+const double &Stopwatch::stoppedTimeAt(const std::vector<double>::size_type index)
+{
+	if (index > stoppedTimes.size()) {
+		return lastStoppedTime();
+	}
+	std::mutex mut;
+	{
+		std::lock_guard<std::mutex> lock(mut);
+		return stoppedTimes.at(index);
+	}
+}
+
+const std::vector<double>& Stopwatch::getStoppedTimes()
+{
+	return stoppedTimes;
+}
+
---
+++ src/blackmetal/src/stopwatch/Stopwatch.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <chrono>
+#include <vector>
+using namespace std::chrono_literals;
+
+/**
+ * @class Stopwatch
+ * @brief Benchmark the program using the RAII procedure.
+ *
+ * Use this class in separate scope with what you want to measure. The class will than save
+ * the measured time in a static vector. You can later access the times separately
+ * or the vector as a whole "const vector&" using static methods.
+ * TODO: The possibility of measuring the time in different units.
+ * Default microseconds. This can be done using templates while checking the type in constructor.
+ */
+class Stopwatch
+{
+public:
+	/**
+	 * @brief Constructor saves a starting timestamp.
+	 */
+	Stopwatch();
+
+	/**
+	 * @brief Destructor calculates the time of its life and saves the time in microseconds (double)
+	 * to static vector.
+	 */
+	~Stopwatch();
+
+	/**
+	 * @brief Access the last stopped time in the vector.
+	 *
+	 * @return Const double reference to the last stopped time.
+	 */
+	static const double &lastStoppedTime();
+
+	/**
+	 * @brief Access a specified stopped time in the vector.
+	 *
+	 * @param index Index of the timestamp to access.
+	 * @return The const double reference to the timestamp.
+	 */
+	static const double &stoppedTimeAt(const std::vector<double>::size_type index);
+
+	/**
+	 * @brief Get the const double reference to all the timestamps.
+	 */
+	static const std::vector<double> &getStoppedTimes();
+private:
+	/// Starting timestamp.
+	std::chrono::system_clock::time_point m_start;
+};
+
