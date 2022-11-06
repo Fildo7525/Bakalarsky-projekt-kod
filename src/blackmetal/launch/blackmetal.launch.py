051032a76a243e82e825c1f591cdb79f254fd4a2
Filip Lobpreis <f.lobpreis@gmail.com>
2022-11-06 15:51:08 +0100

    config: adjust launch file
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/blackmetal/launch/blackmetal.launch.py
+++ src/blackmetal/launch/blackmetal.launch.py
@@ -1,18 +1,32 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+def getConfigFileLocation(file):
+	return os.path.join(
+		get_package_share_directory("blackmetal"),
+		"config",
+		file
+	)
+
 def generate_launch_description():
+	config_blackmetal = getConfigFileLocation("blackmetal.yaml")
+	config_log = getConfigFileLocation("bm_logger.yaml")
+
 	return LaunchDescription([
 		Node(
 			package='blackmetal',
 			executable='blackmetal',
 			output='screen',
+			parameters = [config_blackmetal],
 			emulate_tty=True
 		),
 		Node(
 			package='blackmetal',
 			executable='log_server',
 			output='screen',
+			parameters = [config_log],
 			emulate_tty=True
 		)
 	])
