726fc20349e2f2d36b2bd0381d846fb0855e1dca
Filip Lobpreis <f.lobpreis@gmail.com>
2022-11-27 18:36:22 +0100

    queue: add comments
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/blackmetal/src/communication/queue/Queue.hpp
+++ src/blackmetal/src/communication/queue/Queue.hpp
@@ -6,28 +6,71 @@
 #include <mutex>
 #include <vector>
 
+/**
+ * @brief ts is a namespace grouping thread safe classes and functions.
+ */
 namespace ts
 {
 
+/**
+ * @class Queue
+ * @brief Thread safe dynamic priority queue storing json strings
+ * that are to be send to the desired IP address. The preferred strings
+ * are those that are setting the robot left and right wheel speed.
+ * The ordering is done using the callable object std::greater<std::string>()
+ */
 class Queue {
-	// Moved out of public interface to prevent races between this
-	// and pop().
+	/**
+	 * @brief Checks if the queue is empty.
+	 *
+	 * Moved out of public interface to prevent races between this
+	 * and pop().
+	 *
+	 * @return true if the queue is empty, false otherwise.
+	 */
 	bool empty() const;
 
 public:
+	/**
+	 * @brief Default constructor.
+	 */
 	Queue() = default;
+
+	/**
+	 * @brief The class is not copyable.
+	 */
 	Queue(const Queue &) = delete;
 	Queue& operator=(const Queue &) = delete;
  
+	/**
+	 * @brief Returns the size of the priority queue.
+	 */
 	unsigned long size() const;
+
+	/**
+	 * @brief Returns the top element in the priority queue.
+	 */
 	std::string pop();
+
+	/**
+	 * @brief Add a new element to the priority queue.
+	 *
+	 * Inserts the element to the queue. If the inserted item @param item 
+	 * is sending request for setting the left and right wheel speed
+	 * prioritize this request.
+	 *
+	 * @param item to be added to the queue.
+	 */
 	void push(const std::string &item);
 
 private:
+	/// Priority queue containing the requests to be send.
 	std::priority_queue<std::string, std::vector<std::string>, std::greater<std::string>> m_pqueue;
-	// std::queue<std::string> m_queue;
+	/// The queue mutex.
 	mutable std::mutex m_qMutex;
+	/// Mutex activated in the communication methods.
 	mutable std::mutex m_communicationMutex;
+	/// Condition variable ensuring the pop method will wait for an element if the queue is empty.
 	std::condition_variable m_cvPush;
 };
 
