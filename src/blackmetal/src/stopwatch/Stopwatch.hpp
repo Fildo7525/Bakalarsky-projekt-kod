48f6c03b04a2fa51bd80641f14f0369b3a6467d0
Filip Lobpreis <f.lobpreis@gmail.com>
2023-05-21 14:58:02 +0200

    cp: stopwatch: comments
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- src/blackmetal/src/stopwatch/Stopwatch.hpp
+++ src/blackmetal/src/stopwatch/Stopwatch.hpp
@@ -11,7 +11,7 @@ using namespace std::chrono_literals;
  *
  * Use this class in separate scope with what you want to measure. The class will than save
  * the measured time in a static vector. You can later access the times separately
- * or the vector as a whole "const vector&" using static methods.
+ * or you can access the vector as a whole using static methods.
  * TODO: The possibility of measuring the time in different units.
  * Default microseconds. This can be done using templates while checking the type in constructor.
  */
@@ -19,37 +19,36 @@ class Stopwatch
 {
 public:
 	/**
-	 * @brief Constructor saves a starting timestamp.
+	 * Constructor saves a starting timestamp.
 	 *
 	 * @param maxLength The maximal number of timestamps this instance will allow.
 	 */
 	Stopwatch(size_t maxLength = 1000);
 
 	/**
-	 * @brief Destructor calculates the time of its life and saves the time in microseconds (double)
+	 * Destructor calculates the time of its life and saves the time in microseconds (double)
 	 * to static vector.
 	 */
 	~Stopwatch();
 
 	/**
-	 * @brief Access the last stopped time in the vector.
+	 * Access the last stopped time in the vector of stopped times.
 	 *
-	 * @return Copied double of the last stopped time.
+	 * @return Copy of the last stopped time.
 	 */
 	static double lastStoppedTime();
 
 	/**
-	 * @brief Access a specified stopped time in the vector.
+	 * @brief Access a specified stopped time.
 	 * If the index parameter is bigger than the size of the vector the last stopped time is returned.
 	 *
-	 * @param index Index of the timestamp to access.
+	 * @param index Index of the timestamp to access. To access the last parameter enter -1.
 	 * @return The const double reference to the timestamp.
 	 */
 	static double stoppedTimeAt(const std::vector<double>::size_type index);
 
 	/**
-	 * @brief Get the copy of all the stopped times. The vector capturing them
-	 * is cleared.
+	 * Get the copy of all the stopped times. The vector capturing them is cleared.
 	 */
 	static std::vector<double> getStoppedTimes();
 
